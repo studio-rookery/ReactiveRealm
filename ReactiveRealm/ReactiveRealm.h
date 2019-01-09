e3dc20d0a2e770d33378faf3075b97c4129ce02f
hirano masaki <hirano@fenrir.co.jp>
2019-01-09 17:36:11 +0900

    setup project

---
+++ ReactiveRealm/ReactiveRealm.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveRealm.h
+//  ReactiveRealm
+//
+//  Created by hirano masaki on 2019/01/09.
+//  Copyright © 2019 ReactiveRealm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveRealm.
+FOUNDATION_EXPORT double ReactiveRealmVersionNumber;
+
+//! Project version string for ReactiveRealm.
+FOUNDATION_EXPORT const unsigned char ReactiveRealmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveRealm/PublicHeader.h>
+
+
