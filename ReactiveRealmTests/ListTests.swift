fd816bf5ddbc466fc3ada12a41e0ecebdb0004d7
hirano masaki <hirano@fenrir.co.jp>
2019-01-30 17:26:23 +0900

    Add ListTests

---
+++ ReactiveRealmTests/ListTests.swift
@@ -0,0 +1,70 @@
+//
+//  ListTests.swift
+//  ReactiveRealmTests
+//
+//  Created by hirano masaki on 2019/01/30.
+//  Copyright © 2019 ReactiveRealm. All rights reserved.
+//
+
+import XCTest
+import Result
+import ReactiveSwift
+import RealmSwift
+@testable import ReactiveRealm
+
+final class ListTests: XCTestCase {
+
+    func testProperty() {
+        let realm = Realm.inMemory()
+        
+        let person = Person()
+        
+        try! realm.write {
+            realm.add(person)
+        }
+        
+        let list = person.dogs.reactive.property
+        XCTAssert(list.value.isEmpty)
+        
+        let dogs = [Dog(), Dog()]
+        
+        wait(list.signal, after: {
+            try! realm.write {
+                person.dogs.append(objectsIn: dogs)
+            }
+        })
+        
+        XCTAssertEqual(list.value.count, dogs.count)
+        XCTAssert(list.value[0].isSameObject(as: dogs[0]))
+        XCTAssert(list.value[1].isSameObject(as: dogs[1]))
+        
+        wait(list.signal, after: {
+            try! realm.write {
+                person.dogs.remove(at: 1)
+            }
+        })
+        
+        XCTAssertEqual(list.value.count, 1)
+        XCTAssert(list.value[0].isSameObject(as: dogs[0]))
+    }
+}
+
+extension XCTestCase {
+    
+    @discardableResult
+    func wait<T, E>(_ signal: Signal<T, E>, timeout: TimeInterval = 1, name: String = #function, after action: () -> Void) -> Result<T, E>? {
+        let exp = expectation(description: name)
+        var result: Result<T, E>?
+        
+        signal.take(first: 1).observeResult {
+            result = $0
+            exp.fulfill()
+        }
+        
+        action()
+        
+        waitForExpectations(timeout: timeout, handler: nil)
+        
+        return result
+    }
+}
