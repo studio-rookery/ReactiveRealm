6449c0d25ad5e530b9a70b8f5b8dc427ceee1ad6
hirano masaki <hirano@fenrir.co.jp>
2019-01-30 16:45:55 +0900

    Confirm `Object` to `ReactiveExtensionsProvider`

--- ReactiveRealm/ObjectObservable.swift
+++ ReactiveRealm/ObjectObservable.swift
@@ -19,7 +19,7 @@ public protocol ObjectObservable {
     func observe(_ block: @escaping (ObjectChange) -> ()) -> NotificationTokenType
 }
 
-extension Object: ObjectObservable {
+extension Object: ObjectObservable, ReactiveExtensionsProvider {
     
 }
 
